4bd3186f11044ecc86e680f68d0422ab282a6bf8
Kishor <kishor.koreinfo@gmail.com>
2025-12-10 16:13:27 +0530

    resolved typo whiteNoiseMiddlewre

--- PiDiF/PiDiF/deployment.py
+++ PiDiF/PiDiF/deployment.py
@@ -15,7 +15,7 @@ SECRET_KEY = MY_SECRET_KEY
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'whitenoise.middleware.WhitenoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
